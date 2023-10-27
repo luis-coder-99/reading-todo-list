6d1aa6b5326731efc7a70b84d5495a69def53d48
Luis Hacker <luis.goetz@code.berlin>
2023-10-27 11:22:55 +0200

    correct name

--- reading-todo.py
+++ reading-todo.py
@@ -6,5 +6,5 @@ daily_media = {
     4: "Wirtschaftswoche",
     5: "Manager Magazin"
 }
-### Dictionry to save the time and status of the first dictionary 
+### Dictionary to save the time and status of the first dictionary 
 task = {media_tracking {"completed": False, "start_time": None, "end_time": None, "total_time": 0.0} for service in news_services. values()}
