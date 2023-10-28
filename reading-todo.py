266592a74f22deb2dcdabe3aff56d99ad24aed11
Luis Hacker <luis.goetz@code.berlin>
2023-10-28 20:36:54 +0200

    change seconad dictionary again

--- reading-todo.py
+++ reading-todo.py
@@ -7,4 +7,5 @@ daily_media = {
     5: "Manager Magazin"
 }
 #### Second Dictionary to save the time and status of the first
-task = {media_tracking {"completed": False, "start_time": None, "end_time": None, "total_time": 0.0} for service in news_services. values()}
+task = {service: {"completed": False, "start_time": None, "end_time": None, "total_time": 0.0} for service in daily_media.values()}
+print(task)
